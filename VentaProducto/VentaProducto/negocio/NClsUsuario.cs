82d2eb55b732092148aca38e107f348a170154a4
AdamarsD <ad0094032021@unab.edu.sv>
2021-09-13 16:27:35 -0600

    Create NClsUsuario.cs

---
+++ VentaProducto/VentaProducto/negocio/NClsUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaProducto.negocio
+{
+    class NClsUsuario
+    {
+        public String nombre(NClsUsuario usuario)
+
+        {
+            return " usuario Adamaris "; 
+
+        }
+
+        public int clave(NClsUsuario usuario) => " ingrese la contraseña ";
+
+    }
+}
