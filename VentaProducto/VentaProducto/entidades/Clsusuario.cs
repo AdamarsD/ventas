8c4c5142df0e87ba4bab3588a7ae3c1122bd863e
AdamarsD <ad0094032021@unab.edu.sv>
2021-09-13 16:07:06 -0600

    Create Clsusuario.cs

---
+++ VentaProducto/VentaProducto/entidades/Clsusuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaProducto.entidades
+{
+    class Clsusuario
+    {
+
+        private String nombre;
+        private String clave;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Clave { get => clave; set => clave = value; }
+    }
+}
